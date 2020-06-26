53b4153b48e53eb210b821744dc626027f14800c
Themba Mdluli <tmdluli@c4r6s6.wethinkcode.co.za>
2020-06-26 09:18:46 +0200

    finished ex01

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmdluli <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 09:17:47 by tmdluli           #+#    #+#             */
+/*   Updated: 2020/06/26 09:17:51 by tmdluli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+
+# define FT_BOOLEAN_H
+# define TRUE 1
+# define FALSE 0
+# define EVEN(x) ((x) % 2)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+# include <unistd.h>
+
+typedef int    t_bool;
+
+#endif
+
