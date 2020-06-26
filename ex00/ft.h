294d58a5156df33566ff67c96febdeb41229f75f
Themba Mdluli <tmdluli@c4r6s6.wethinkcode.co.za>
2020-06-26 09:18:22 +0200

    finished ex00

---
+++ ex00/ft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmdluli <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 09:17:28 by tmdluli           #+#    #+#             */
+/*   Updated: 2020/06/26 09:17:31 by tmdluli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+
+#define FT_H
+
+void ft_putchar(char c);
+void ft_putstr(char *str);
+void ft_swap(int *a, int*b);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2)
+
+#endif
